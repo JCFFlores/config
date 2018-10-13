c360255ec0d924bb203d8ee4c6592eebdd14a0cb
José Flores <josecruzflores.fl@gmail.com>
2018-10-13 14:12:02 -0500

    Add config.fish

---
+++ config.fish
@@ -0,0 +1,61 @@
+function fish_greeting
+    set_color brgreen
+    fortune
+    set_color normal
+end
+
+set -x MANPATH /usr/share/fish/man /usr/local/man /usr/local/share/man /usr/share/man
+
+set -x PIPENV_SHELL_FANCY 1
+
+# pyenv config
+set -x PYENV_ROOT $HOME/.pyenv
+set -x PATH $PYENV_ROOT/bin $PATH
+
+function cd
+    builtin cd $argv
+    ls
+end
+
+function home
+    cd ~
+end
+
+function ..
+    cd ../
+end
+
+function top
+    htop
+end
+
+# Emacs ansi-term support
+if test -n "$EMACS"
+    set -x TERM eterm-color
+end
+
+# This function may be required
+function fish_title
+    true
+end
+
+# Runs the command on the background with no hangup
+# Redirects stdout and stderr to /dev/null
+function nh
+    nohup $argv > /dev/null 2>&1 &
+end
+
+# Run google-chrome with netflix open
+function netflix
+    nh google-chrome --app=http://netflix.com 
+end
+
+function cowfortune
+    fortune | cowsay | lolcat
+end
+
+# pyenv configuration
+pyenv init - | source 
+
+# make fuck callable
+thefuck --alias | source
