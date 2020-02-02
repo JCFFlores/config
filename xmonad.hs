62b170cfd50389bfd79c763d7bc0a14a24c34904
Jose Flores <josecruzflores.fl@gmail.com>
2020-02-01 23:17:24 -0600

    Use only Tall and Full layout

--- xmonad.hs
+++ xmonad.hs
@@ -2,6 +2,7 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Util.EZConfig
+import XMonad.Layout.NoBorders
 
 keyBindings :: [(String, X ())]
 keyBindings = [ ("<XF86AudioMute>", spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle")
@@ -18,5 +19,6 @@ main = xmonad =<< xmobar (ewmh def
     terminal = "urxvtopen -e fish"
     , modMask = mod4Mask
     , borderWidth = 3
+    , layoutHook = Tall 1 (3/100) (1/2) ||| noBorders Full
     , handleEventHook = handleEventHook def <+> fullscreenEventHook
   } `additionalKeysP` keyBindings)
